826a61c0a7557beabc27d35cfe3432c57190b493
zio4272 <zio4272@naver.com>
2017-10-16 10:04:00 +0900

    LGTwin 앱 초기준비 완료 스플 메인

---
+++ app/src/main/java/kr/co/tjeit/lgtwins/BaseActivity.java
@@ -0,0 +1,21 @@
+package kr.co.tjeit.lgtwins;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by the on 2017-10-16.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void bindView();
+
+    public abstract void setupEvent();
+
+    public abstract void setValues();
+
+
+}
---
+++ app/src/main/java/kr/co/tjeit/lgtwins/MainActivity.java
@@ -0,0 +1,31 @@
+package kr.co.tjeit.lgtwins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindView();
+        setupEvent();
+        setValues();
+    }
+
+    @Override
+    public void bindView() {
+
+    }
+
+    @Override
+    public void setupEvent() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
