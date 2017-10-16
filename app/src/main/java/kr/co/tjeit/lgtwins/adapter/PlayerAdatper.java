1f5325c86a7e30f89bcd05ba3659c83ae6fd2f62
zio4272 <zio4272@naver.com>
2017-10-16 18:20:25 +0900

    PagerSlidingTabStrip 진행중

--- app/src/main/java/kr/co/tjeit/lgtwins/adapter/PlayerAdatper.java
+++ app/src/main/java/kr/co/tjeit/lgtwins/adapter/PlayerAdatper.java
@@ -28,7 +28,6 @@ public class PlayerAdatper extends ArrayAdapter<Player> {
         mContext = context;
         mList = list;
         inf = LayoutInflater.from(mContext);
-
     }
 
     @NonNull
--- app/src/main/java/kr/co/tjeit/lgtwins/fragment/PlayerFragment.java
+++ app/src/main/java/kr/co/tjeit/lgtwins/fragment/PlayerFragment.java
@@ -1,13 +1,16 @@
 package kr.co.tjeit.lgtwins.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +27,24 @@ import kr.co.tjeit.lgtwins.util.GlobalData;
 public class PlayerFragment extends Fragment {
 
 
-
     private android.widget.ListView playerListView;
     PlayerAdatper mAdapter;
     List<Player> players = new ArrayList<>();
+    private PagerSlidingTabStrip tabStrip;
+    private ViewPager viewPager;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_player, container, false);
+        this.viewPager = (ViewPager) v.findViewById(R.id.viewPager);
+        this.tabStrip = (PagerSlidingTabStrip) v.findViewById(R.id.tabStrip);
+
+
         this.playerListView = (ListView) v.findViewById(R.id.playerListView);
+
+
         return v;
     }
 
@@ -47,6 +58,8 @@ public class PlayerFragment extends Fragment {
     private void setValues() {
 
 
+//        viewPager.setAdapter(new PlayerAdatper(getContext(), ));
+        tabStrip.setViewPager(viewPager);
 
         mAdapter = new PlayerAdatper(getContext(), players);
         playerListView.setAdapter(mAdapter);
