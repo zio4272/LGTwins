2fb86bd4de462b0d092f23bd3a380c2db98bdc00
zio4272 <zio4272@naver.com>
2017-10-19 17:04:19 +0900

    대상 홈페이지 html로 일단 불러오기는 성공

--- app/src/main/java/kr/co/tje/lgtwins/TeamRankActivity.java
+++ app/src/main/java/kr/co/tje/lgtwins/TeamRankActivity.java
@@ -2,16 +2,28 @@ package kr.co.tje.lgtwins;
 
 import android.Manifest;
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import kr.co.tje.lgtwins.util.PermissionRequester;
 
@@ -19,13 +31,18 @@ import kr.co.tje.lgtwins.util.PermissionRequester;
 public class TeamRankActivity extends BaseActivity {
 
 
+    String urlAddress = "http://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo&year=2017";
+    Handler handler = new Handler(); // 화면에 그려주기 위한 객체
+
+
     private TextView testTxt;
+    private android.widget.Button btn;
+    private TextView Txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_rank);
-
         bindView();
         setupEvent();
         setValues();
@@ -39,37 +56,68 @@ public class TeamRankActivity extends BaseActivity {
                 activity.finish();
             }
         });
+
+
     }
-
-
-    private class MainPageTask extends AsyncTask<Void, Void, Void> {
-        private Elements element;
-
-        @Override
-        protected void onPostExecute(Void result) {
-            //doInBackground 작업이 끝나고 난뒤의 작업
-            testTxt.setText(element.text());
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            //백그라운드 작업이 진행되는 곳.
-            try {
-                Document doc = Jsoup.connect("http://sports.news.naver.com").get();
-                element = doc.select("#algoList > tbody");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
-
 
 
     @Override
     public void setupEvent() {
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadHtml();
+            }
+        });
+
     }
+
+    private void loadHtml() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuffer sb = new StringBuffer();
+
+                try {
+                    URL url = new URL(urlAddress);
+                    HttpURLConnection conn =
+                            (HttpURLConnection) url.openConnection();// 접속
+                    if (conn != null) {
+                        conn.setConnectTimeout(2000);
+                        conn.setUseCaches(false);
+                        if (conn.getResponseCode()
+                                == HttpURLConnection.HTTP_OK) {
+                            //    데이터 읽기
+                            BufferedReader br
+                                    = new BufferedReader(new InputStreamReader
+                                    (conn.getInputStream(), "UTF-8"));//"utf-8"
+                            while (true) {
+                                String line = br.readLine();
+                                if (line == null) break;
+                                sb.append(line + "\n");
+                            }
+                            br.close(); // 스트림 해제
+                        }
+                        conn.disconnect(); // 연결 끊기
+                    }
+                    // 값을 출력하기
+                    Log.d("test", sb.toString());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Txt.setText(sb.toString());
+
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start(); // 쓰레드 시작
+    }
+
 
     @Override
     public void setValues() {
@@ -78,7 +126,8 @@ public class TeamRankActivity extends BaseActivity {
 
     @Override
     public void bindView() {
-        this.testTxt = (TextView) findViewById(R.id.testTxt);
+        this.Txt = (TextView) findViewById(R.id.Txt);
+        this.btn = (Button) findViewById(R.id.btn);
 
     }
 
