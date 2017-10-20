e00bc01dad856fe4204582eaf936a0c57bb9eeba
zio4272 <zio4272@naver.com>
2017-10-20 11:41:07 +0900

    jsoup 팀순위 테스팅중

--- app/src/main/java/kr/co/tje/lgtwins/TeamRankActivity.java
+++ app/src/main/java/kr/co/tje/lgtwins/TeamRankActivity.java
@@ -2,11 +2,8 @@ package kr.co.tje.lgtwins;
 
 import android.Manifest;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,107 +12,70 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import kr.co.tje.lgtwins.util.PermissionRequester;
 
 
 public class TeamRankActivity extends BaseActivity {
+    private android.widget.TextView text;
 
-
-    String urlAddress = "http://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo&year=2017";
-    Handler handler = new Handler(); // 화면에 그려주기 위한 객체
-
-
-    private TextView testTxt;
-    private android.widget.Button btn;
-    private TextView Txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_rank);
+
         bindView();
         setupEvent();
         setValues();
 
 
-        PermissionRequester.Builder request = new PermissionRequester.Builder(this);
-        request.create().request(Manifest.permission.INTERNET, 10000, new PermissionRequester.OnClickDenyButtonListener() {
-            @Override
-            public void onClick(Activity activity) {
-                Toast.makeText(activity, "인터넷 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
-                activity.finish();
+    }
+
+    private class naverBaseballTeamRank extends AsyncTask<Void, Void, Map<String, String>> {
+
+        @Override
+        protected Map<String, String> doInBackground(Void... params) {
+            Map<String, String> result = new HashMap<String, String>();
+            try {
+                Document document = Jsoup.connect("http://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo").get();
+                Elements teamTable = document.select("#regularTeamRecordList_table");
+                Elements rows = teamTable.select("tr");
+
+//                result.put("팀테이블", teamTable.toString());
+
+                for (int i = 1; i < rows.size(); i++) {
+                    Element row = rows.get(i);
+                    result.put("tr값", row.toString());
+
+                    Elements cols = row.select("td");
+                    result.put("td값", cols.text());
+                }
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+            return result;
+        }
 
+        @Override
+        protected void onPostExecute(Map<String, String> map) {
+            text.setText(map.get("전부"));
 
+        }
     }
 
 
     @Override
     public void setupEvent() {
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadHtml();
-            }
-        });
 
-    }
 
-    private void loadHtml() {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final StringBuffer sb = new StringBuffer();
-
-                try {
-                    URL url = new URL(urlAddress);
-                    HttpURLConnection conn =
-                            (HttpURLConnection) url.openConnection();// 접속
-                    if (conn != null) {
-                        conn.setConnectTimeout(2000);
-                        conn.setUseCaches(false);
-                        if (conn.getResponseCode()
-                                == HttpURLConnection.HTTP_OK) {
-                            //    데이터 읽기
-                            BufferedReader br
-                                    = new BufferedReader(new InputStreamReader
-                                    (conn.getInputStream(), "UTF-8"));//"utf-8"
-                            while (true) {
-                                String line = br.readLine();
-                                if (line == null) break;
-                                sb.append(line + "\n");
-                            }
-                            br.close(); // 스트림 해제
-                        }
-                        conn.disconnect(); // 연결 끊기
-                    }
-                    // 값을 출력하기
-                    Log.d("test", sb.toString());
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Txt.setText(sb.toString());
-
-                        }
-                    });
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t.start(); // 쓰레드 시작
     }
 
 
@@ -126,9 +86,7 @@ public class TeamRankActivity extends BaseActivity {
 
     @Override
     public void bindView() {
-        this.Txt = (TextView) findViewById(R.id.Txt);
-        this.btn = (Button) findViewById(R.id.btn);
-
+        this.text = (TextView) findViewById(R.id.text);
     }
 
 
